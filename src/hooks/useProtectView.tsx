3fb1fb777906184a0f02dc7cc00cd12e7d3e5df4
Andres Gonzalez <andrfgr@gmail.com>
2023-08-25 21:30:54 +1000

    add refactor

--- src/hooks/useProtectView.tsx
+++ src/hooks/useProtectView.tsx
@@ -3,14 +3,14 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const useProtectView = (location: string) => {
-  const { data, status } = useSession();
+  const { status } = useSession(); //data,
   const router = useRouter();
 
   useEffect(() => {
     if (status === "unauthenticated") {
       void router.push(location);
     }
-  }, [status]);
+  }, [status, router, location]);
   return status;
 };
 
