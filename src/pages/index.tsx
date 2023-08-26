93e7f101e9c5baf31a67f2e9957e99b9c6dfeca7
Andres Gonzalez <andrfgr@gmail.com>
2023-08-26 21:50:50 +1000

    update index by mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,7 +76,7 @@ export default function AuthenticationPage({
           className="hidden dark:block"
         />
       </div> */}
-      <div className="container relative hidden h-[1000px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative h-[1000px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         {/* <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" size="icon">
@@ -106,7 +106,7 @@ export default function AuthenticationPage({
         >
           Login
         </Link> */}
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div className="relative h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-primary" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
@@ -135,7 +135,7 @@ export default function AuthenticationPage({
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="mt-8 lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
