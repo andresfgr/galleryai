2a32a6515baed34f37266bee6a4a536999150ff9
Andres Gonzalez <andrfgr@gmail.com>
2023-08-26 20:54:39 +1000

    add refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,59 @@
+"use client";
+
+import * as React from "react";
+
+import { cn } from "~/lib/utils";
+import { Icons } from "~/components/ui/icons";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>;
+
 // import Image from "next/image";
 import Link from "next/link";
 
-import { cn } from "~/lib/utils";
-import { buttonVariants } from "~/components/ui/button";
-import UserAuthForm from "./authentication/components/userAuthForm";
+// import { cn } from "~/lib/utils";
+// import { buttonVariants } from "~/components/ui/button";
+// import UserAuthForm from "./authentication/components/userAuthForm";
 // import { ThemeProvider } from "~/components/theme-provider";
 
-export default function AuthenticationPage() {
+export default function AuthenticationPage({
+  className,
+  ...props
+}: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  const { data: sessionData } = useSession();
+  const router = useRouter();
+
+  // const handleGithubLogin = async () => {
+  //   await signIn("github", { callbackUrl: "/" });
+  // };
+
+  const handleGoogleLogin = async () => {
+    await signIn("google", { callbackUrl: "/" });
+  };
+
+  function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  useEffect(() => {
+    if (sessionData) {
+      void router.push("/generate/page");
+    }
+  }, [sessionData, router]);
+
   //{ children }: any
   return (
     <>
@@ -50,7 +97,7 @@ export default function AuthenticationPage() {
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu> */}
-        <Link
+        {/* <Link
           href="/authentication/page"
           className={cn(
             buttonVariants({ variant: "ghost" }),
@@ -58,7 +105,7 @@ export default function AuthenticationPage() {
           )}
         >
           Login
-        </Link>
+        </Link> */}
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-primary" />
           <div className="relative z-20 flex items-center text-lg font-medium">
@@ -98,7 +145,69 @@ export default function AuthenticationPage() {
                 Enter your email below to create your account
               </p>
             </div>
-            <UserAuthForm />
+
+            <div className={cn("grid gap-6", className)} {...props}>
+              <form onSubmit={onSubmit}>
+                <div className="grid gap-2">
+                  <div className="grid gap-1">
+                    <Label className="sr-only" htmlFor="email">
+                      Email
+                    </Label>
+                    <Input
+                      id="email"
+                      placeholder="name@example.com"
+                      type="email"
+                      autoCapitalize="none"
+                      autoComplete="email"
+                      autoCorrect="off"
+                      disabled={isLoading}
+                    />
+                  </div>
+                  <Button disabled={isLoading}>
+                    {isLoading && (
+                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                    )}
+                    Sign In with Email
+                  </Button>
+                </div>
+              </form>
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+              <Button
+                variant="outline"
+                type="button"
+                disabled={isLoading}
+                onClick={
+                  sessionData
+                    ? () => void signOut()
+                    : () => void handleGoogleLogin()
+                }
+              >
+                <Icons.google className="mr-2 h-4 w-4" />{" "}
+                {sessionData ? " Sign out" : " Google"}
+              </Button>
+              {/* <Button
+        variant="outline"
+        type="button"
+        disabled={isLoading}
+        onClick={
+          sessionData ? () => void signOut() : () => void handleGithubLogin()
+        }
+      >
+        <Icons.gitHub className="mr-2 h-4 w-4" />{" "}
+        {sessionData ? " Sign out" : " Github"}
+      </Button> */}
+            </div>
+
+            {/* <UserAuthForm /> */}
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{" "}
               <Link
